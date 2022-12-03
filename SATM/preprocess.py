4e6d24011d31770fdbdeed1f002f720928366ad0
tancredimarciano <tancredimarciano@gmail.com>
2022-12-03 18:01:57 +0100

    update

--- SATM/preprocess.py
+++ SATM/preprocess.py
@@ -57,8 +57,7 @@ def merge_aves_df(df1 = "train_2017_bboxes.json", df2 = "val_2017_bboxes.json"):
 
 
 
-category_list = os.listdir("Aves")
-def clean_aves(path = "Aves", element = ".ipynb_checkpoints", cat_list = category_list):
+def clean_aves(cat_list, path = "Aves", element = ".ipynb_checkpoints"):
     for cat in cat_list:
         try:
             shutil.rmtree(path+"/"+cat+"/"+element)
@@ -92,3 +91,58 @@ def encode_df(df, var_name = "category_id"):
      'Ardea herodias': 10}
     df[var_name] = df.category_name.apply(lambda x: encode_cat_dic[x])
     return df
+
+
+
+def convert_to_yolov5(which, df):
+    print_buffer = []
+    input_path = os.listdir("data/images/"+which)
+    for path in tqdm(input_path):
+        
+        temp_df = df[df.identifier == path][["category_id","bbox","width", "height"]]
+        #print(path)
+        with open("data/labels/"+which+"/"+path[:-3]+"txt", "w") as fp:
+            for i in range(len(temp_df)):
+                class_id = temp_df.iloc[i].category_id - 1
+                
+                
+                b_x, b_y, b_width, b_height  = temp_df.iloc[i].bbox
+                b_center_x = b_x + b_width/2
+                b_center_y = b_y + b_height/2
+                
+                
+                image_w, image_h = temp_df[["width", "height"]].iloc[0]
+                b_center_x /= image_w 
+                b_center_y /= image_h 
+                b_width    /= image_w 
+                b_height   /= image_h 
+                
+                line = "{} {:.3f} {:.3f} {:.3f} {:.3f}".format(class_id, b_center_x, b_center_y, b_width, b_height)
+                #print(line)
+                fp.writelines(line+"\n")
+                
+                
+                
+                
+def generate_txt(identifier, transformed, v):
+    boxes = transformed["bboxes"]
+    labels = [i.item() -1  for i in transformed["labels"]]
+    with open("data_over/labels/Train/"+identifier[:-4]+v+".txt", "w") as fp:
+        for i in range(len(boxes)):
+            class_id = labels[i]
+
+
+            b_x, b_y, b_width, b_height  = boxes[i]
+            b_center_x = b_x + b_width/2
+            b_center_y = b_y + b_height/2
+
+            image_w, image_h = train_df[train_df.identifier == identifier][["width", "height"]].iloc[0]
+
+            b_center_x /= image_w 
+            b_center_y /= image_h 
+            b_width    /= image_w 
+            b_height   /= image_h 
+
+            line = "{} {:.3f} {:.3f} {:.3f} {:.3f}".format(class_id, b_center_x, b_center_y, b_width, b_height)
+            #print(line)
+            fp.writelines(line+"\n")
